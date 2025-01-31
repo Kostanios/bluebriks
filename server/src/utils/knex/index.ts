a47cbfe50b7934fccb99f6b1ecbcf8ede9f43a1d
KonstantinKotau <kotov.const@gmail.com>
2025-01-31 16:19:14 +0100

    documentation

--- server/src/utils/knex/index.ts
+++ server/src/utils/knex/index.ts
@@ -6,13 +6,13 @@ dotenv.config();
 const {
   POSTGRES_USER,
   POSTGRES_PASSWORD,
-  DB_HOST = "0.0.0.0",
+  POSTGRES_HOST = "0.0.0.0",
   DB_NAME = "postgres",
 } = process.env;
 
 const pgknex = knex({
   client: "pg",
-  connection: `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${DB_HOST}:5432/${DB_NAME}`,
+  connection: `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${DB_NAME}`,
   searchPath: ["knex", "public"],
 });
 
